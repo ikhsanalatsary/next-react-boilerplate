c23f115214e70ddad112b5481688374a783381ff
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2017-10-21 14:36:28 +0700

    (feat) disable redux tool on production

--- store.js
+++ store.js
@@ -1,5 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware, compose } from 'redux';
 import withRedux from 'next-redux-wrapper';
 import nextReduxSaga from 'next-redux-saga';
 import createSagaMiddleware from 'redux-saga';
@@ -14,11 +13,19 @@ const root = () => (combineReducers({
   global: rootReducer,
 }));
 
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+
+/* eslint-enable */
 export function configureStore(initialState = {}) {
   const store = createStore(
     root(),
     fromJS(initialState),
-    composeWithDevTools(applyMiddleware(sagaMiddleware))
+    composeEnhancers(applyMiddleware(sagaMiddleware))
   );
 
   store.sagaTask = sagaMiddleware.run(rootSaga);
